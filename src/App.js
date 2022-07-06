45c2d78b8ff73af488897bc54eb4811992440e37
Durga Prasad Narikalapa <ndurgaprasad10@gmail.com>
2022-07-06 14:00:42 +0530

    Added support for images and clean plugin setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Recipes from "./Recipies";
 
 import "./styles/index.scss";
 
 const App = () => {
-    return (<h1>App</h1>)
+    return (
+        <>
+            <section className="hero"></section>
+            <main>
+                <section>
+                    <h1>Hi, React</h1>
+                </section>
+            </main>
+            <Recipes />
+        </>
+    );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin =  require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 MiniCssExtractPlugin
 
-let mode  = "development";
+let mode = "development";
 
-if(process.env.NODE_ENV ===  'production') {
-    mode = 'production';
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
 }
 
 module.exports = {
     mode: mode,
 
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        assetModuleFilename: "images/[hash][ext][query]"
+    },
+
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: {
@@ -22,17 +29,34 @@ module.exports = {
             },
             {
                 test: /\.(s[ac|c])ss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
-            }
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ""
+                        },
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+                type: "asset"
+            },
         ]
     },
 
     plugins: [
-        new MiniCssExtractPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
     ],
 
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: [".js", ".jsx"],
     },
 
     devtool: "source-map",
