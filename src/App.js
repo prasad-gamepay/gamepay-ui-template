d287416a22c8788852d9fe06c864b5ee97575ba6
Durga Prasad Narikalapa <ndurgaprasad10@gmail.com>
2022-07-09 21:35:15 +0530

    tailwind css integrated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,32 @@ const App = () => {
   return (
     <main>
       <section>
-        <h1>Hi, React</h1>
+        <div class="container flex justify-center py-20">
+          <div class="p-3 bg-white rounded-xl max-w-xl hover:shadow">
+            <div class="flex justify-between w-full">
+              <img
+                src="https://avatars.githubusercontent.com/u/7972572?v=4"
+                width="250"
+                class="rounded-lg"
+              />
+              <div class="ml-2">
+                <div class="p-3">
+                  <h3 class="text-2xl">Durga Prasad Narikalapa</h3>
+                  <span>Fullstack Developer</span>
+                </div>
+
+                <div class="flex justify-between items-center mt-2 gap-2">
+                  <button class="w-full h-12 rounded-md border-2 text-md hover:shadow hover:bg-red-700 hover:border-red-700 hover:text-white ">
+                    Linked In
+                  </button>
+                  <button class="w-full h-12 rounded-md bg-blue-700 text-white text-md hover:shadow hover:bg-blue-800">
+                    GitHub
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin =  require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {
+    CleanWebpackPlugin
+} = require("clean-webpack-plugin");
 
 let mode = "development";
 
